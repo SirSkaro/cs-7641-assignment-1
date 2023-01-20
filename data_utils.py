242f35e203c55898c71f20ef71e3eda731184f5e
unknown <bencbchurchill@gmail.com>
2023-01-20 14:06:53 -0800

    Initial attempt of training a decision-tree classifier

--- data_utils.py
+++ data_utils.py
@@ -4,24 +4,24 @@ from enum import Enum
 import numpy
 
 
-class DataSet(Enum):
+class Task(Enum):
     LETTER_RECOGNITION = ('letter recognition', 0, numpy.arange(1, 17, dtype=int), int)  # directory, index of label, data columns, data type
     SCRIBE_RECOGNITION = ('scribe recognition', 10, numpy.arange(0, 10, dtype=int), float)
 
 
-def parse_data(dataset: DataSet) -> Tuple[numpy.ndarray, numpy.array]:
-    filename = './datasets/' + dataset.value[0] + '/data'
-    data = numpy.loadtxt(
+def parse_data(task: Task) -> Tuple[numpy.ndarray, numpy.array]:
+    filename = './datasets/' + task.value[0] + '/data'
+    dataset = numpy.loadtxt(
         fname=filename,
         delimiter=',',
-        usecols=dataset.value[2],
-        dtype=dataset.value[3]
+        usecols=task.value[2],
+        dtype=task.value[3]
     )
     labels = numpy.loadtxt(
         fname=filename,
         delimiter=',',
-        usecols=dataset.value[1],
+        usecols=task.value[1],
         dtype=str
     )
 
-    return data, labels
+    return dataset, labels
--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,17 @@
+import data_utils
+from data_utils import Task
+
 from sklearn import tree
 
 
 ### Docs used:
 # https://scikit-learn.org/stable/modules/tree.html
 # https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html#sphx-glr-auto-examples-tree-plot-cost-complexity-pruning-py
+
+def learn(task: Task):
+    dataset, labels = data_utils.parse_data(task)
+    training_set = dataset[:20000]
+    test_set = dataset[20000:]
+
+    classifier = tree.DecisionTreeClassifier()
+    return classifier.fit(training_set, labels)
