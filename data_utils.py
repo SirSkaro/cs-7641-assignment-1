f882ac2b8fea565eed0cf84a0447599302ab0ec9
unknown <bencbchurchill@gmail.com>
2023-01-20 14:56:24 -0800

    Finished initial decision tree learner

--- data_utils.py
+++ data_utils.py
@@ -9,6 +9,15 @@ class Task(Enum):
     SCRIBE_RECOGNITION = ('scribe recognition', 10, numpy.arange(0, 10, dtype=int), float)
 
 
+class SampleSet:
+    def __init__(self, samples, labels):
+        self.samples = samples
+        self.labels = labels
+
+    def size(self) -> int:
+        return self.labels.size
+
+
 def parse_data(task: Task) -> Tuple[numpy.ndarray, numpy.array]:
     filename = './datasets/' + task.value[0] + '/data'
     dataset = numpy.loadtxt(
@@ -25,3 +34,18 @@ def parse_data(task: Task) -> Tuple[numpy.ndarray, numpy.array]:
     )
 
     return dataset, labels
+
+
+def partition_samples(samples: numpy.ndarray, labels: numpy.array):
+    sample_count = labels.size
+    training_set_size = int(sample_count * 0.9)
+
+    training_set = SampleSet(samples[:training_set_size], labels[:training_set_size])
+    test_set = SampleSet(samples[training_set_size:], labels[training_set_size:])
+    return training_set, test_set
+
+
+def get_training_and_test_sets(task: Task) -> Tuple[SampleSet, SampleSet]:
+    samples, labels = parse_data(task)
+    return partition_samples(samples, labels)
+
--- decision_tree.py
+++ decision_tree.py
@@ -2,6 +2,7 @@ import data_utils
 from data_utils import Task
 
 from sklearn import tree
+import numpy as np
 
 
 ### Docs used:
@@ -9,9 +10,11 @@ from sklearn import tree
 # https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html#sphx-glr-auto-examples-tree-plot-cost-complexity-pruning-py
 
 def learn(task: Task):
-    dataset, labels = data_utils.parse_data(task)
-    training_set = dataset[:20000]
-    test_set = dataset[20000:]
+    training_set, test_set = data_utils.get_training_and_test_sets(task)
 
-    classifier = tree.DecisionTreeClassifier()
-    return classifier.fit(training_set, labels)
+    classifier = tree.DecisionTreeClassifier().fit(training_set.samples, training_set.labels)
+
+    correctly_classified_count = np.sum(test_set.labels == classifier.predict(test_set.samples))
+    error = 1.0 - (correctly_classified_count / test_set.size())
+
+    return classifier, error
