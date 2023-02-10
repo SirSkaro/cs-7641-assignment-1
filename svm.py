18caf48dc4b0801ac2a9cd43fccd7009603b36a8
unknown <bencbchurchill@gmail.com>
2023-02-09 20:11:52 -0800

    Implemented most of SCM

--- svm.py
+++ svm.py
@@ -4,6 +4,7 @@ from data_utils import Task, SampleSet
 from sklearn.svm import SVC
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
 ### Docs used:
 # https://scikit-learn.org/stable/modules/svm.html#svm
@@ -30,34 +31,103 @@ def learn(task: Task):
     training_set, test_set = data_utils.get_training_and_test_sets(task)
 
     classifier = SVC(
-        kernel=POLYNOMIAL,
-        degree=3,   # degree of the polynomial - ignored by all other kernels
+        kernel=RADIAL_BASIS_FUNCTION,
         gamma='auto',
-        tol=1e-3,
+        C=5.0,    # small = underfit, large = overfit
         decision_function_shape=ONE_VS_ONE,
-        random_state=0
+        random_state=0,
+        cache_size=2000  # in MB
     )
 
     classifier.fit(training_set.samples, training_set.labels)
-
     error = 1.0 - classifier.score(test_set.samples, test_set.labels)
-
     return classifier, error
 
 
-def plot(clf, training_set: SampleSet):
-    # The equation of the separating plane is given by all x so that np.dot(svc.coef_[0], x) + b = 0.
-    # Solve for w3 (z)
-    z = lambda x, y: (-clf.intercept_[0] - clf.coef_[0][0] * x - clf.coef_[0][1] * y) / clf.coef_[0][2]
-    X, Y = training_set.samples, training_set.labels
+def weighted_shuffle(task: Task, kernel: str, C: float, percent_training: float = 0.90):
+    training_set, test_set = data_utils.get_training_and_test_sets(task, percent_training=percent_training, randomize=True)
+    classifier = SVC(
+        kernel=kernel,
+        C=C,
+        gamma='auto',
+        degree=5,
+        class_weight='balanced',
+        decision_function_shape=ONE_VS_ONE,
+        random_state=0,
+        cache_size=2000  # in MB
+    )
 
-    tmp = np.linspace(-5, 5, 30)
-    x, y = np.meshgrid(tmp, tmp)
+    classifier.fit(training_set.samples, training_set.labels)
+    error = 1.0 - classifier.score(test_set.samples, test_set.labels)
+    train_error = 1.0 - classifier.score(training_set.samples, training_set.labels)
+    return classifier, error, train_error
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot3D(X[Y == 0, 0], X[Y == 0, 1], X[Y == 0, 2], 'ob')
-    ax.plot3D(X[Y == 1, 0], X[Y == 1, 1], X[Y == 1, 2], 'sr')
-    ax.plot_surface(x, y, z(x, y))
-    ax.view_init(30, 60)
-    plt.show()
+
+def iterate_weighted_shuffle(task: Task, percent_training: float = 0.90):
+    kernels = [LINEAR, POLYNOMIAL, RADIAL_BASIS_FUNCTION]
+    Cs = [5, 20, 100]
+    candidate_classifiers = {
+        LINEAR: (None, 1.0, 1.0),
+        POLYNOMIAL: (None, 1.0, 1.0),
+        RADIAL_BASIS_FUNCTION: (None, 1.0, 1.0)
+    }
+
+    for C in Cs:
+        for kernel in kernels:
+            print(f'Training "{kernel}" kernel with C={C}...')
+            start = time()
+            classifier, error, train_error = weighted_shuffle(task, kernel, C, percent_training)
+            end = time()
+            training_time = round(end - start, 2)
+            print(f'\tFinished in {training_time} seconds with error {error}')
+            if error < candidate_classifiers[kernel][1]:
+                candidate_classifiers[kernel] = (classifier, error, train_error, training_time)
+                print(f'\tNew best classifier for kernel')
+
+    return candidate_classifiers
+
+
+def create_learning_curve():
+    kernels = [LINEAR, POLYNOMIAL, RADIAL_BASIS_FUNCTION]
+    fig, ax = plt.subplots(1, 2)
+
+    ax[0].set_title("Learning Curve")
+    ax[0].set_xlabel("Percentage Training Set")
+    ax[0].set_ylabel("Error")
+
+    ax[1].set_title("Training Time")
+    ax[1].set_xlabel("Percentage Training Set")
+    ax[1].set_ylabel("Training Time (in Seconds)")
+
+    percentages = np.linspace(0, 1, 11)[1:-1]
+    for task, name, linestyle in [(Task.SCRIBE_RECOGNITION, 'Scribe', 'dotted'),
+                                         (Task.LETTER_RECOGNITION, 'Letter', 'dashed')]:
+        test_errors = {
+            LINEAR: [],
+            POLYNOMIAL: [],
+            RADIAL_BASIS_FUNCTION: []
+        }
+        train_errors = {
+            LINEAR: [],
+            POLYNOMIAL: [],
+            RADIAL_BASIS_FUNCTION: []
+        }
+        training_times = {
+            LINEAR: [],
+            POLYNOMIAL: [],
+            RADIAL_BASIS_FUNCTION: []
+        }
+        for percent_training in percentages:
+            classifiers = iterate_weighted_shuffle(task, percent_training=percent_training)
+            for kernel in kernels:
+                test_errors[kernel].append(classifiers[kernel][1])
+                train_errors[kernel].append(classifiers[kernel][2])
+                training_times[kernel].append(classifiers[kernel][3])
+
+        for kernel in kernels:
+            ax[0].plot(percentages, test_errors[kernel], label=f'{name} Test Error - {kernel.upper()}', marker="o", drawstyle="steps-post", linestyle=linestyle)
+            ax[0].plot(percentages, train_errors[kernel], label=f'{name} Train Error - {kernel.upper()}', marker="o", drawstyle="steps-post", linestyle=linestyle)
+            ax[1].plot(percentages, training_times[kernel], label=f'{name} Classifier - {kernel.upper()}', marker="o", drawstyle="steps-post", linestyle=linestyle)
+
+    ax[0].legend(loc="best")
+    ax[1].legend(loc="best")
